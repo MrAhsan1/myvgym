912f4f257a24a61424710fd562e8fe4f9694324d
MrAhsan1 <ahsan@bbits.solutions>
2020-11-28 12:58:49 +0500

    d

--- server/db.js
+++ server/db.js
@@ -6,10 +6,9 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://postgres:12345@localhost:5432/myvgym';
+// var connectionString = 'postgres://postgres:12345@localhost:5432/myvgym';
 
-// var connectionString = 'postgres://cennduoixlsclu:981bdd6e18cd1acd982873624470b8fffb6218ebd21c060a774dcc7bcff02380@'+
-'ec2-35-168-77-215.compute-1.amazonaws.com:5432/davqvjlbqgbfth';
+var connectionString = 'postgres://cennduoixlsclu:981bdd6e18cd1acd982873624470b8fffb6218ebd21c060a774dcc7bcff02380@ec2-35-168-77-215.compute-1.amazonaws.com:5432/davqvjlbqgbfth';
 const db = pgp(connectionString);
 
 module.exports= {
